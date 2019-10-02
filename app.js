a382b1dc7fa2f7a110aec2baed3d2869c3d64b45
JazzGlobal <waymer147@gmail.com>
2019-10-01 22:13:21 -0400

    added mongoose, app, and passport configuration

--- app.js
+++ app.js
@@ -6,12 +6,39 @@ var express = require('express'),
     User = require('./models/user'),
     app = express()
 
+// MONGOOSE CONFIG
+mongoose.connect(process.env.MONGOD_URI || 'mongodb+srv://jazzglobal:Coolkids1478!@cluster0-cvxgu.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+
+//PASSPORT CONFIG
+passport.serializeUser((user,done)=>{
+    done(null,user);
+});
+
+passport.deserializeUser((obj,done)=>{
+    done(null,obj);
+});
+
+app.use(require("express-session")({
+    secret: 'i am root',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     res.redirect('/home');
-})
+});
 
 app.get('/home', (req, res) => {
-    res.render('home.ejs')
+    res.render('home')
 });
 
 try {
